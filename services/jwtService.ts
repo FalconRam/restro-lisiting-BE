2770d8994e3ddd8e6349b332767c5b241a4e46ef
FalconRam <ramvignesh974@gmail.com>
2024-03-22 22:29:47 +0400

    refrac: secret keys accessed through env

--- services/jwtService.ts
+++ services/jwtService.ts
@@ -5,12 +5,18 @@ export const signJWTToken = async (
   payload: customJWTPayload
 ): Promise<string> => {
   try {
-    const accessToken = await jwt.sign(payload, "tempSec", {
-      expiresIn: "2hr",
-    });
+    if (!process.env.JWT_TOKEN_SECRET_KEY) throw new Error("JWT token not set");
+
+    const accessToken = await jwt.sign(
+      payload,
+      process.env.JWT_TOKEN_SECRET_KEY,
+      {
+        expiresIn: process.env.JWT_TOKEN_EXPIRY_HOURS,
+      }
+    );
     return accessToken;
   } catch (error: any) {
-    return error.messsage || error.stack || error;
+    throw new Error(error.messsage || error.stack || error);
   }
 };
 
